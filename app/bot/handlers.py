0c6d3549f2b9182e1a3b391ff56f9f6e2dd2c223
Ivan-49 <remnevvanya@yandex.ru>
2025-01-23 16:34:30 +0300

    handlers

--- app/bot/handlers.py
+++ app/bot/handlers.py
@@ -1,26 +1,11 @@
-from aiogram import types, F
-from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import State, StatesGroup
-from .keyboards import get_product_keyboard
-from .utils import fetch_product_details
+from app.utils import fetch_product_details
+async def get_product_info(artikul: str):
+    try:
+        product_details = await fetch_product_details(artikul)
+        if product_details is None:
+            return None
+        return product_details
+    except Exception as e:
+        print(f"Error in get_product_info: {e}")
+        return None
 
-class Form(StatesGroup):
-    artikul = State()
-
-async def get_product_data(message: types.Message, state: FSMContext):
-    await state.set_state(Form.artikul)
-    await message.reply("Пожалуйста, введите артикул товара.")
-
-async def process_artikul(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['artikul'] = message.text
-    product_details = await fetch_product_details(data['artikul'])
-    if product_details:
-        await message.reply(f"Данные по товару:\nНазвание: {product_details['name']}\nЦена: {product_details['price']} RUB\nРейтинг: {product_details['rating']}\nКоличество: {product_details['total_quantity']}")
-    else:
-        await message.reply("Товар не найден.")
-    await state.clear()
-
-def register_handlers(dp):
-     dp.message.register(get_product_data, F.text.contains("Получить данные по товару"))
-     dp.message.register(process_artikul, Form.artikul)
