a7c08a94d5a6a860ad47c5d3bd5b5492730ef130
Ivan-49 <remnevvanya@yandex.ru>
2025-01-23 14:52:46 +0300

    products and subscribe

--- app/routers/products.py
+++ app/routers/products.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, Depends, HTTPException
-from typing import List
 from app import crud, schemas
 from app.database import async_sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -11,20 +10,7 @@ async def get_db():
     async with async_sessionmaker() as session:
         yield session
 
-@router.post("/products", response_model=schemas.Product)
+@router.post("/api/v1/products", response_model=schemas.Product)
 async def create_product(product: schemas.ProductCreate, session: AsyncSession = Depends(get_db)):
     db_product = await crud.create_product(session, product)
-    return db_product
-
-@router.get("/products", response_model=List[schemas.Product])
-async def read_products(session: AsyncSession = Depends(get_db)):
-    db_products = await crud.read_products(session)
-    return db_products
-
-
-@router.get("/products/{product_id}", response_model=schemas.Product)
-async def read_product(product_id: int, session: AsyncSession = Depends(get_db)):
-    db_product = await crud.read_product(session, product_id)
-    if db_product is None:
-        raise HTTPException(status_code=404, detail="Product not found")
     return db_product
--- app/routers/subscribe.py
+++ app/routers/subscribe.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, Depends, HTTPException
-from typing import List
 from app import crud, schemas
 from app.database import async_sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -11,21 +10,9 @@ async def get_db():
     async with async_sessionmaker() as session:
         yield session
 
-@router.post("/subscribe", response_model=schemas.Subscribe)
-async def create_subscribe(subscribe: schemas.SubscribeCreate, session: AsyncSession = Depends(get_db)):
-    db_subscribe = await crud.create_subscribe(session, subscribe)
-    return db_subscribe
-
-
-@router.get("/subscribe", response_model=List[schemas.Subscribe])
-async def read_subscribes(session: AsyncSession = Depends(get_db)):
-    db_subscribe = await crud.read_subscribes(session)
-    return db_subscribe
-
-
-@router.get("/subscribe/{subscribe_id}", response_model=schemas.Subscribe)
-async def read_subscribe(subscribe_id: int, session: AsyncSession = Depends(get_db)):
-    db_subscribe = await crud.read_subscribe(session, subscribe_id)
-    if db_subscribe is None:
-        raise HTTPException(status_code=404, detail="Subscribe not found")
-    return db_subscribe
+@router.get("/api/v1/subscribe/{artikul}", response_model=schemas.Product)
+async def read_product_by_artikul(artikul: int, session: AsyncSession = Depends(get_db)):
+    db_product = await crud.read_product_by_artikul(session, artikul)
+    if db_product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return db_product
