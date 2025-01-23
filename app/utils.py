725335c50b22b04229a17bd621349369d83e293d
Ivan-49 <remnevvanya@yandex.ru>
2025-01-23 15:32:05 +0300

    utils

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,6 @@
 import aiohttp
+from fastapi import HTTPException
+import app.schemas as schemas
 
 async def fetch_product_details(artikul: str):
     url = f"https://card.wb.ru/cards/v1/detail?appType=1&curr=rub&dest=-1257786&sp=30&nm={artikul}"
@@ -6,13 +8,19 @@ async def fetch_product_details(artikul: str):
         async with session.get(url) as response:
             if response.status == 200:
                 data = await response.json()
-                product_info = {
-                    "name": data.get("data", {}).get("products", [{}])[0].get("name"),
-                    "artikul": artikul,
-                    "price": data.get("data", {}).get("products", [{}])[0].get("salePriceU") / 100,
-                    "rating": data.get("data", {}).get("products", [{}])[0].get("rating"),
-                    "total_quantity": data.get("data", {}).get("products", [{}])[0].get("sizes", [{}])[0].get("stocks", 0)
-                }
-                return product_info
+                products = data.get("data", {}).get("products", [])
+                if products:
+                    product = products[0]
+                    sizes = product.get("sizes", [])
+                    stocks = sizes[0].get("stocks", 0) if sizes else 0 #Обработка случая отсутствия sizes
+                    return schemas.Product(
+                        name=product.get("name", ""),  # Предотвращение ошибок при отсутствии name
+                        artikul=artikul,
+                        price=product.get("salePriceU", 0) / 100, # Предотвращение ошибок при отсутствии salePriceU
+                        rating=product.get("rating", 0.0),  # Предотвращение ошибок при отсутствии rating
+                        total_quantity=stocks #Предотвращение ошибки при отсутствии stocks
+                    )
+                else:
+                    raise HTTPException(status_code=404, detail="Product not found on Wildberries")
             else:
-                return None
+                raise HTTPException(status_code=response.status, detail=f"Wildberries API request failed with status code {response.status}")
